0bb013b78f6a7d4404d9ba9d66f636f92af95c03
Isaacrgz <isaacrgz97@gmail.com>
2023-10-29 14:03:36 -0600

    Second commit

--- src/main/java/com/examplegithub/App.java
+++ src/main/java/com/examplegithub/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! este batch fue creado desde vscode" );
     }
 }
